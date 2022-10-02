270364f1e0e88a7f0d5f9b21357b74350d32caad
seacat271 <seacat271083@gmail.com>
2022-10-02 21:09:30 +0300

    add state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
-export const increment = createAction("myValue/increment");
-export const decrement = createAction("myValue/decrement");
+export const addContact = createAction("myContacts/addContact");
+export const deleteContact = createAction("myContacts/deleteContact");
 
-const myReducer = createReducer(0, {
-    [increment]: (state, action) => state + action.payload,
-    [decrement]: (state, action) => state - action.payload,
+const myReducer = createReducer([], {
+    [addContact]: (state, action) => [...state, action.payload],
+    [deleteContact]: (state, action) => state.filter(item => item.id !== action.payload),
 
 })
 
