d216f0e2c6596421b531772beb53bcce9bf17c7f
seacat271 <seacat271083@gmail.com>
2022-10-02 20:28:14 +0300

    increment/decrement

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+const increment = createAction("myValue/increment");
+const decrement = createAction("myValue/decrement");
+
+const myReducer = createReducer(10, {
+    [increment]: (state, action) => state + action.payload,
+    [decrement]: (state, action) => state - action.payload,
+
+})
+
+
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        myValue: myReducer,
+    },
 })
