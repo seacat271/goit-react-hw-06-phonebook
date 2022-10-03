9b2777d2240363e8d687eddae608665d1e204886
seacat271 <seacat271083@gmail.com>
2022-10-03 10:13:38 +0300

    add combine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,7 @@ import Section from './Section/Section';
 import Filter from './Filter/Filter';
 import { ContainerGlobal } from './App.styled';
 
-
-// const useLocalStorage = (key, initialValue) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(key)) ?? initialValue;})
-
-//   useEffect(() => {
-//     window.localStorage.setItem(key, JSON.stringify(state))
-//   }, [key, state])
-//   return [state, setState]
-// }
-
 export const App = () => {
-// const [contacts, setContacts] = useLocalStorage("contacts", []);
 
   return (
     <ContainerGlobal>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { configureStore} from "@reduxjs/toolkit";
 import { contactsSlice } from "./contacts/slice";
 import { filterSlice } from "./filter/slice";
+import { combineReducers } from "@reduxjs/toolkit";
 
 
+
+
+const rootReducer = combineReducers({contacts: contactsSlice.reducer, filter: filterSlice.reducer,})
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsSlice.reducer,
-        filter: filterSlice.reducer,
-    },
+    reducer: rootReducer,
 })
-
