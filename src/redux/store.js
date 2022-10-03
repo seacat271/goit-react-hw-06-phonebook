46c505aa61a1ce4593686e90d163225759584e0f
seacat271 <seacat271083@gmail.com>
2022-10-03 10:34:06 +0300

    add whitelist

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key: 'root',
     storage,
+    blacklist: ['filter'],
   }
 const rootReducer = combineReducers({contacts: contactsSlice.reducer, filter: filterSlice.reducer,})
 
